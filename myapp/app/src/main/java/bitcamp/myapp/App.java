d50ab8b95b3bd8fcf21e043f3707042aad67a92c
fantasyshrimp <fantasy.shrimp@gmail.com>
2022-12-30 11:15:16 +0900

    java study

--- myapp/app/src/main/java/bitcamp/myapp/App.java
+++ myapp/app/src/main/java/bitcamp/myapp/App.java
@@ -3,64 +3,90 @@
  */
 package bitcamp.myapp;
 
+import java.sql.Date;
 import java.util.Scanner;
 
 public class App {
   public static void main(String[] args) {
 
     Scanner keyScanner = new Scanner(System.in);
+    final int ACCOUNT_SIZE = 50;
+    int count = 0;
 
-    System.out.print("번호 : ");
-    int no = Integer.parseInt(keyScanner.nextLine());
-    System.out.print("이름 : ");
-    String name = keyScanner.nextLine();
-    System.out.print("전화번호(010-1234-5678) : ");
-    String tell = keyScanner.nextLine();
-    System.out.print("우편번호 : ");
-    String postNo = keyScanner.nextLine();
-    System.out.print("주소 : ");
-    String basicAddress = keyScanner.nextLine();
-    System.out.print("상세주소 : ");
-    String detaileAddress = keyScanner.nextLine();
-    System.out.print("재직여부 (true / false) : ");
-    boolean working = Boolean.parseBoolean(keyScanner.nextLine());
-    System.out.print("성별 (M / W) : ");
-    char gender = keyScanner.nextLine().charAt(0); // M(남자) W(여자)
-    System.out.print("전공여부 (0: 비전공, 1: 준전공, 2: 전공) : ");
-    byte level = Byte.parseByte(keyScanner.nextLine()); // 0(비전공)
-    System.out.print("가입일(yyyy-mm-dd) : ");
-    String createDate = keyScanner.nextLine();
+    int[] nos = new int[ACCOUNT_SIZE];
+    String[] names = new String[ACCOUNT_SIZE];
+    String[] tells = new String[ACCOUNT_SIZE];
+    String[] postNos = new String[ACCOUNT_SIZE];
+    String[] basicAddresses = new String[ACCOUNT_SIZE];
+    String[] detaileAddresses = new String[ACCOUNT_SIZE];
 
-    System.out.print("\n\n\n");
+    boolean[] workings = new boolean[ACCOUNT_SIZE];
+    char[] genders = new char[ACCOUNT_SIZE];
+    byte[] levels = new byte[ACCOUNT_SIZE];
+    String[] createDates = new String[ACCOUNT_SIZE];
 
-    System.out.printf("번호: %d\n", no);
-    System.out.printf("이름: %s\n", name);
-    System.out.printf("전화번호: %s\n", tell);
-    System.out.printf("우편번호: %s\n", postNo);
+    for (int i = 0; i < ACCOUNT_SIZE; i++) {
+      System.out.print("번호 : ");
+      nos[i] = Integer.parseInt(keyScanner.nextLine());
+      System.out.print("이름 : ");
+      names[i] = keyScanner.nextLine();
+      System.out.print("전화번호(010-1234-5678) : ");
+      tells[i] = keyScanner.nextLine();
+      System.out.print("우편번호 : ");
+      postNos[i] = keyScanner.nextLine();
+      System.out.print("주소 : ");
+      basicAddresses[i] = keyScanner.nextLine();
+      System.out.print("상세주소 : ");
+      detaileAddresses[i] = keyScanner.nextLine();
+      System.out.print("재직여부 (0: 미취업 / 1. 재직중) : ");
+      workings[i] = Integer.parseInt(keyScanner.nextLine()) == 1;
+      System.out.print("성별 (M / W) : ");
+      genders[i] = keyScanner.nextLine().charAt(0); // M(남자) W(여자)
+      System.out.print("전공여부 (0: 비전공, 1: 준전공, 2: 전공) : ");
+      levels[i] = Byte.parseByte(keyScanner.nextLine()); // 0(비전공)
+      Date today = new Date(System.currentTimeMillis());
+      createDates[i] = today.toString();
 
-    System.out.printf("성별: %s\n", gender == 'M' ? "남자" : "여자");
-    System.out.printf("주소: %s\n", basicAddress);
-    System.out.printf("상세주소: %s\n", detaileAddress);
 
-    System.out.printf("재직여부: %s\n", working ? "예" : "아니오");
+      count++;
 
-    switch (level) {
-      case 0:
-        System.out.printf("전공: %s\n", "비전공자");
+      System.out.print("계속 입력하시겠습니까? Y / n");
+      String str = keyScanner.nextLine();
+
+      if (!str.equalsIgnoreCase("Y") && str.length() != 0) {
         break;
-      case 1:
-        System.out.printf("전공: %s\n", "준정공자");
-        break;
-      case 2:
-        System.out.printf("전공: %s\n", "전공자");
-        break;
-      default:
+      }
     }
 
+    for (int i = 0; i < count; i++) {
 
-    System.out.printf("가입일: %s\n", createDate);
+      System.out.print("\n\n\n");
 
+      System.out.printf("번호: %d\n", nos[i]);
+      System.out.printf("이름: %s\n", names[i]);
+      System.out.printf("전화번호: %s\n", tells[i]);
+      System.out.printf("우편번호: %s\n", postNos[i]);
 
+      System.out.printf("성별: %s\n", genders[i] == 'M' ? "남자" : "여자");
+      System.out.printf("주소: %s\n", basicAddresses[i]);
+      System.out.printf("상세주소: %s\n", detaileAddresses[i]);
 
+      System.out.printf("재직여부: %s\n", workings[i] ? "미취업" : "재직중");
+
+      switch (levels[i]) {
+        case 0:
+          System.out.printf("전공: %s\n", "비전공자");
+          break;
+        case 1:
+          System.out.printf("전공: %s\n", "준정공자");
+          break;
+        case 2:
+          System.out.printf("전공: %s\n", "전공자");
+          break;
+        default:
+      }
+
+      System.out.printf("가입일: %s\n", createDates[i]);
+    }
   }
 }
